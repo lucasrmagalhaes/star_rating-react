14e6e77dcc269485506f5cdb388cb4840f44e02d
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-08 09:04:14 -0300

    useState Colors Star

--- react-star-rating/src/StarRating.js
+++ react-star-rating/src/StarRating.js
@@ -1,10 +1,29 @@
+import { useState } from 'react'
 import { FaStar } from 'react-icons/fa'
 
 const StarRating = () => {
+    const [rating, setRating] = useState(null)
+
     return (
         <div>
-            {[...Array(5)].map(star => {
-                return <FaStar size={100} />
+            {[...Array(5)].map((star, i) => {
+                const ratingValue = i + 1
+
+                return (
+                    <label>
+                        <input 
+                            type="radio" 
+                            name="rating" 
+                            value={ratingValue} 
+                            onClick={() => setRating(ratingValue)} 
+                        />
+                        <FaStar 
+                            class="star" 
+                            color={ratingValue <= rating ? "#ffc107" : "#e4e5e9"} 
+                            size={100} 
+                        />
+                    </label>
+                )
             })}
         </div>
     )        
